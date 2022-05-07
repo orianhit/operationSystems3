91de9cc7862a9e0de1746e87a13f372b9948decd
orian <orianhit@hit.ac.il>
2022-05-07 19:32:28 +0300

    actual shared memory + using sem for notify al done

--- main.c
+++ main.c
@@ -1,47 +1,96 @@
 #include <stdio.h>
+#include <sys/mman.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/sem.h>
+#define MAX_NUMER 100
+
+union semun
+{
+    int val;               /* Value for SETVAL */
+    struct semid_ds *buf;  /* Buffer for IPC_STAT, IPC_SET */
+    unsigned short *array; /* Array for GETALL, SETALL */
+    struct seminfo *__buf; /* Buffer for IPC_INFO
+                              (Linux-specific) */
+};
 
 sem_t mutex;
-int sum = 0;
+static int *sum;
 
-
-void sem_action(int current_number) {
-    sum += current_number;
-}
-  
-void* thread(void* arg)
+void sem_action(int current_number)
 {
-    int current_number = *((int*)arg);
+    *sum += current_number;
+    printf("proc\t%d\tsum\t%d\n", current_number, *sum);
+}
 
+void wait_until_value_max(int semid)
+{
+    struct sembuf *sops = (struct sembuf *)malloc(1 * sizeof(struct sembuf *));
+    sops[0].sem_num = 0;
+    sops[0].sem_flg = 0;
+    sops[0].sem_op = -100;
+
+    semop(semid, sops, 1);
+    free(sops);
+}
+
+void increase_sem(int semid)
+{
+    struct sembuf *sops = (struct sembuf *)malloc(1 * sizeof(struct sembuf *));
+    sops[0].sem_num = 0;
+    sops[0].sem_flg = 0;
+    sops[0].sem_op = 1;
+
+    semop(semid, sops, 1);
+    free(sops);
+}
+
+int init_sems()
+{
+    union semun smearg1;
+    int semid = semget(IPC_PRIVATE, 1, 0600);
+    smearg1.val = 0;
+
+    semctl(semid, 1, SETVAL, smearg1);
+    return semid;
+}
+
+void calc_sum(int semid, int current_number)
+{
     sem_wait(&mutex);
 
     sem_action(current_number);
-        
+
     sem_post(&mutex);
+    increase_sem(semid);
 }
-  
-  
+
 int main()
 {
-    int MAX_NUMER = 100;
     sem_init(&mutex, 0, 1);
 
-    pthread_t t[MAX_NUMER];
-    int threadNum[MAX_NUMER];
-    for (int i=0; i< MAX_NUMER; i++)
-        threadNum[i] = i + 1;
+    sum = mmap(NULL, sizeof *sum, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    *sum = 0;
 
-    for(int i=0;i<MAX_NUMER;i++) {
-        pthread_create(&t[i],NULL,thread,&threadNum[i]);
+    int semid = init_sems();
+
+    for (int i = 1; i <= MAX_NUMER; i++)
+    {
+        if (fork() == 0)
+        {
+            calc_sum(semid, i);
+            exit(1);
+        }
     }
-    for(int i=0;i<MAX_NUMER;i++)
-        pthread_join(t[i],NULL);
 
+    wait_until_value_max(semid);
     sem_destroy(&mutex);
 
-    printf("the total sum is %d \n", sum);
+    printf("the total sum is %d \n", *sum);
+    munmap(sum, sizeof *sum);
+    exit(1);
 
     return 0;
 }
